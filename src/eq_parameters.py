4ec033be15ea137835d9d931d316d344928c8392
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2024-01-20 14:28:16 -0300

    parameters of the equation

---
+++ src/eq_parameters.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+def alpha_m(voltage: float) -> float:
+    """Computes the alpha_m parameter of the Hodgkin-Huxley model, given
+    by alpha_m = 0.1(V + 25)/(e^((V + 25)/10) - 1)
+
+    Args:
+        voltage (float): voltage V in the neuron
+
+    Returns:
+        float: the alpha_m parameter for the specified voltage 
+    """
+    V = voltage
+    alpha = 0.1*(V + 25)/(np.exp((V + 25)/10) - 1)
+    
+    return alpha
+
+
+def beta_m(voltage: float) -> float:
+    """Computes the beta_m parameter of the Hodgkin-Huxley model, given
+    by beta_m = 4e^(V/18)
+
+    Args:
+        voltage (float): voltage V in the neuron
+
+    Returns:
+        float: the beta_m parameter for the specified voltage 
+    """
+    V = voltage
+    beta = 4*np.exp(V/18)
+    
+    return beta
+
+
+def alpha_h(voltage: float) -> float:
+    """Computes the alpha_h parameter of the Hodgkin-Huxley model, given
+    by alpha_h = 0.07e^(V/20)
+
+    Args:
+        voltage (float): voltage V in the neuron
+
+    Returns:
+        float: the alpha_h parameter for the specified voltage 
+    """
+    V = voltage
+    alpha = 0.07*np.exp(V/20)
+    
+    return alpha
+
+
+def beta_h(voltage: float) -> float:
+    """Computes the beta_h parameter of the Hodgkin-Huxley model, given
+    by beta_h = 1/(e^((V + 30)/10) + 1)
+
+    Args:
+        voltage (float): voltage V in the neuron
+
+    Returns:
+        float: the beta_h parameter for the specified voltage 
+    """
+    V = voltage
+    beta = 1/(np.exp((V + 30)/10) + 1)
+    
+    return beta
+
+
+def alpha_n(voltage: float) -> float:
+    """Computes the alpha_n parameter of the Hodgkin-Huxley model, given
+    by alpha_n = 0.01(V + 10)/(e^((V + 10)/10) - 1)
+
+    Args:
+        voltage (float): voltage V in the neuron
+
+    Returns:
+        float: the alpha_n parameter for the specified voltage 
+    """
+    V = voltage
+    alpha = 0.01*(V + 10)/(np.exp((V + 10)/10) - 1)
+    
+    return alpha
+
+
+def beta_n(voltage: float) -> float:
+    """Computes the beta_n parameter of the Hodgkin-Huxley model, given
+    by beta_n = 0.125e^(V/80)
+
+    Args:
+        voltage (float): voltage V in the neuron
+
+    Returns:
+        float: the beta_n parameter for the specified voltage 
+    """
+    V = voltage
+    beta = 0.125*np.exp(V/80)
+    
+    return beta
